da145e4013f47d9da824d4f9602580b51df6697f
Rene Hdz <mcrohs@hotmail.com>
2024-10-04 00:38:06 +0200

    leer variable de entorno PORT

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express, { json } from "express";
 const app = express();
 const contact_routes = require("./routes/app.routes");
+import dotenv from "dotenv";
+dotenv.config();
 
 app.use(express.json());
 
-const PORT = process.env.PORT ?? 3214;
+const PORT = process.env.PORT || 3214;
+
+const BASE_URL = `http://localhost:${PORT}`;
+module.exports={BASE_URL}
 app.listen(PORT,()=>
-    console.log(`Server ready at: http://localhost:${PORT}`)
+    console.log(`Server ready at: ${BASE_URL}`)
 );
 
 app.use("/contacts", contact_routes);
